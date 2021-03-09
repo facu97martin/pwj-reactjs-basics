acffac730787405c1e15ce8219ccbef13e4576e7
Facu Martin <facu97martin@gmail.com>
2021-03-09 20:07:50 -0300

    First react component

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h1>Amazon Cart</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
